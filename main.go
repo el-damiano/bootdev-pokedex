e905611da231f14c1281cab31c88fe69936fc257
el-damiano <el9damiano@gmail.com>
2025-02-23 18:55:14 +0100

    feat: basic repl with help and exit commands

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -11,6 +13,65 @@ func inputClean(text string) []string {
 	return words
 }
 
+type commandREPL struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print(`Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+`)
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	commands := map[string]commandREPL{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Print the Help section",
+			callback:    commandHelp,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := inputClean(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		command, ok := commands[commandName]
+		if !ok {
+			fmt.Print("Unknown command\n")
+			continue
+		}
+
+		err := command.callback()
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
+
+	}
+
 }
